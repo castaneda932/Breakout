5cf8774b3e71b94a1e5ab8ff4ca23b4de50f972c
castaneda932 <castachatz90@outlook.com>
2024-05-18 19:17:43 -0600

    creamos los bloques
    -se crearon bloques con sus scripts

---
+++ Breakout/Assets/_Scripts/BloqueExplosivo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloqueExplosivo : Bloque
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Breakout/Assets/_Scripts/BloquePiedra.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloquePiedra : Bloque  
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        resistencia = 5;
+    }
+
+    public override void RebotarBola()
+    {
+        base.RebotarBola();
+    }
+
+}
