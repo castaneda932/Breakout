a697455f9bbd7e93fea49c0148f0c3f6f0e723af
castaneda932 <castachatz90@outlook.com>
2024-06-17 12:19:59 -0600

    corrección de errores
    - se arreglo lo de agregar el scriptable de opciones.
    -arregle lo del hub de puntaje alto y actual que estaba fuera de pantalla. era porque no tenia bien configurada la posición en canvas.
    -Arregle el tema de la velocidad de la bola y la dirección de inicio.

--- Breakout/Assets/_Scripts/Jugador.cs
+++ Breakout/Assets/_Scripts/Jugador.cs
@@ -24,7 +24,7 @@ public class Jugador : MonoBehaviour
         {
             Vector3 direccion = collision.contacts[0].point - transform.position;
             direccion = direccion.normalized;
-            collision.rigidbody.velocity = collision.gameObject.GetComponent<Bola>().velocidadBola * direccion;
+            collision.rigidbody.velocity = collision.gameObject.GetComponent<Bola>().opciones.velocidadBola * direccion;
         }
 
 
