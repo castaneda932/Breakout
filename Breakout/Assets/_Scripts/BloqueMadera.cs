8afaa8cf16fec20215c5931f43e43b8bed257746
castaneda932 <castachatz90@outlook.com>
2024-06-25 10:43:34 -0600

    actualizacion de codigo
    .

--- Breakout/Assets/_Scripts/BloqueMadera.cs
+++ Breakout/Assets/_Scripts/BloqueMadera.cs
@@ -4,26 +4,13 @@ using UnityEngine;
 
 public class BloqueMadera : Bloque
 {
-    public void CambioDeResistenciaConDificultad()
-    {
-        if (opcionesDelJuego.nivelDificultad == Opciones.dificultad.facil)
-        {
-            this.resistencia = this.resistencia + 1;
-        }
-        else if (opcionesDelJuego.nivelDificultad == Opciones.dificultad.normal)
-        {
-            this.resistencia = this.resistencia + 2;
-        }
-        else if (opcionesDelJuego.nivelDificultad == Opciones.dificultad.dificil)
-        {
-            this.resistencia = this.resistencia * 2;
-        }
-    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       
         resistencia = 3;
+        CambioDeResistenciaConDificultad();
     }
     public override void RebotarBola()
     {
--- Breakout/Assets/_Scripts/BloquePiedra.cs
+++ Breakout/Assets/_Scripts/BloquePiedra.cs
@@ -4,26 +4,14 @@ using UnityEngine;
 
 public class BloquePiedra : Bloque  
 {
-    public void CambioDeResistenciaConDificultad()
-    {
-        if (opcionesDelJuego.nivelDificultad == Opciones.dificultad.facil)
-        {
-            this.resistencia = this.resistencia + 1;
-        }
-        else if (opcionesDelJuego.nivelDificultad == Opciones.dificultad.normal)
-        {
-            this.resistencia = this.resistencia + 2;
-        }
-        else if (opcionesDelJuego.nivelDificultad == Opciones.dificultad.dificil)
-        {
-            this.resistencia = this.resistencia * 2;
-        }
-    }
+    
     // Start is called before the first frame update
     void Start()
     {
         
+
         resistencia = 5;
+        CambioDeResistenciaConDificultad();
     }
 
     public override void RebotarBola()
